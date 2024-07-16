21b9aa01ff2334d1e09263e6ccc18a2a1285a72f
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-07-16 17:58:45 -0300

    feat: add server init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-export function helloWorld() {
-  console.log('Hello, world!');
-}
+import express from 'express';
+import dotenv from 'dotenv';
+import { configureRoutes } from './presentation/routes/auth.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '@/infrastructure/config/swagger/swagger';
 
-helloWorld();
+dotenv.config();
+const app = express();
+app.use(express.json());
+const apiRouter = configureRoutes();
+
+app.use('/api', apiRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
