edcd1c4e39d6fa6e29d2ca88f2c6ff3a26fd63f8
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-07-20 15:44:16 -0300

    feat: add dynamic router config

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { configureRoutes } from './presentation/routes/auth.routes';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '@/infrastructure/config/swagger/swagger';
-
-dotenv.config();
-const app = express();
-app.use(express.json());
-const apiRouter = configureRoutes();
-
-app.use('/api', apiRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+import { createServer } from '@/infrastructure/config/server/server.config';
+import { configureAuthRoutes } from '@/presentation/routes/auth.routes';
 
 const PORT = process.env.PORT || 3000;
+
+const app = createServer([configureAuthRoutes]);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
